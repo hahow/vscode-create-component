99f638bc022ea8265a8ec88d993ed1dcc515fe50
Amo Wu <amo260@gmail.com>
2022-03-05 23:24:03 +0800

    feat: 調整 Component template，用不到的程式碼加 TODO 註解

--- src/templates/component/stories.ts
+++ src/templates/component/stories.ts
@@ -1,4 +1,5 @@
-const STORIES_TEMPLATE = `import { action } from '@storybook/addon-actions';
+const STORIES_TEMPLATE = `// TODO: 如果沒用到 action，可以直接移除程式碼
+// import { action } from '@storybook/addon-actions';
 import type { Meta, Story } from '@storybook/react';
 
 import {{name}} from './{{name}}';
@@ -6,8 +7,10 @@ import type { {{name}}Props } from './{{name}}.type';
 
 export default {
   component: {{name}},
-  decorators: [],
-  parameters: {},
+  // TODO: 如果沒用到 decorators，可以直接移除程式碼
+  // decorators: [],
+  // TODO: 如果沒用到 parameters，可以直接移除程式碼
+  // parameters: {},
   title: '{{name}}',
 } as Meta<{{name}}Props>;
 
@@ -16,11 +19,12 @@ const Template: Story<{{name}}Props> = (args) => <{{name}} {...args} />;
 export const Basic = Template.bind({});
 Basic.args = {};
 Basic.parameters = {
-  design: {
-    type: 'figma',
-    url: '',
-  },
-  // TODO: 如果希望 Percy screenshot，請取消註解；否則請移除程式碼
+  // TODO: 如果沒用到 figma url，可以直接移除程式碼
+  // design: {
+  //   type: 'figma',
+  //   url: '',
+  // },
+  // TODO: 如果希望 Percy screenshot，請取消註解；否則請移除程式碼（預設 skip: true）
   // percy: { skip: false },
 };
 Basic.storyName = '{{name}}';
--- src/templates/component/type.ts
+++ src/templates/component/type.ts
@@ -1,4 +1,6 @@
-const TYPE_TEMPLATE = `export interface {{name}}Props {}
+const TYPE_TEMPLATE = `export interface {{name}}Props {
+  // TODO: Add props type here
+}
 `;
 
 export default TYPE_TEMPLATE;
